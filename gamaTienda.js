aa74ba5988edab06c3c460ef8889934b3291e29d
unknown <nicolas20-20@hotmail.com>
2023-04-03 23:02:36 -0500

    segunda entrega

--- gamaTienda.js
+++ gamaTienda.js
@@ -3,22 +3,46 @@ let nombre = prompt('ingrese su nombre');
 let confirmacion = prompt('¿Desea ver nuestros productos? (s-si / n-no)');
 let eleccion = 0;
 
+let graficas = ["AMD", "NVIDIA"]
+let cpus = ["Intel", "AMD"]
+let monitores = ["LG", "SAMSUNG", "ASUS"]
+
 function calculo(eleccion){
     let total = (eleccion * 0.19) + eleccion;
     return Math.floor(total); 
 }
-while(confirmacion == 's' || confirmacion == 'S'){
-    let producto = prompt(nombre + ' ' + 'estos son nuestros productos: \n-Monitor LG (a) \n-grafica AMD (b) \n-cpu INTEL (C)');
-    if(producto == 'a' || producto == 'A'){
-        alert('Monitor de 200 USD + IVA agregado');
-        eleccion = eleccion + 200;
-    }else if(producto == 'b' || producto == 'B'){
-        alert('Grafica AMD 400 USD + IVA agregada')
-        eleccion = eleccion + 400;
-    }else if(producto == 'c' || producto == 'C'){
-        alert('CPU Intel 250 USD + IVA agregado')
-        eleccion = eleccion + 250;
 
+while(confirmacion == 's' || confirmacion == 'S'){
+    let producto = prompt(nombre + ' ' + 'estos son nuestros productos: \nMonitores (a) \nGraficas (b) \nCpus (C)');
+    if(producto == 'a' || producto == 'A'){
+
+        let confirmacionMonitor = prompt('Elija un monitor: \nLG \nSAMSUNG \nASUS').toUpperCase();
+        if(monitores.indexOf(confirmacionMonitor) !== -1 ){
+            alert('Monitor' + " " + confirmacionMonitor + ' de 300 USD + IVA agregado');
+            monitores.splice(monitores.indexOf(confirmacionMonitor), 1);
+            eleccion = eleccion + 200;
+        }else{
+            alert("no hay en stock")
+        }
+        
+    }else if(producto == 'b' || producto == 'B'){
+        let confirmacionGrafica = prompt('Elija un Grafica: \nAMD \nNVIDIA ').toUpperCase();
+        if(monitores.indexOf(confirmacionGrafica) !== -1 ){
+            alert('Grafica' + " " + confirmacionGrafica + ' de 400 USD + IVA agregada');
+            monitores.splice(monitores.indexOf(confirmacionGrafica), 1);
+            eleccion = eleccion + 400;
+        }else{
+            alert("no hay en stock")
+        }
+    }else if(producto == 'c' || producto == 'C'){
+        let confirmacionCpu = prompt('Elija un un procesador: \nAMD \INTEL ').toUpperCase();
+        if(monitores.indexOf(confirmacionCpu) !== -1 ){
+            alert('Cpu' + " " + confirmacionCpu + ' de 250 USD + IVA agregado');
+            monitores.splice(monitores.indexOf(confirmacionCpu), 1);
+            eleccion = eleccion + 250;
+        }else{
+            alert("no hay en stock")
+        }        
     }else{
         alert('Selecciona un codigo del menu ')
     }
@@ -27,3 +51,23 @@ while(confirmacion == 's' || confirmacion == 'S'){
 
 alert('El precio total con IVA es: ' + '' + calculo(eleccion));
 alert('Gracias por su compra, ' + nombre + '!');
+
+
+const productosMuestra = [
+    {
+        foto: "../img/gpu3.png",
+        nombre: "Grafica AMD",
+        precio: 400
+    },
+    {
+        foto: "../img/monitor1.jpeg",
+        nombre: "Monitor",
+        precio: 200
+    },
+    {
+        foto: "../img/intel.jpg",
+        nombre: "Procesador intel",
+        precio: 250
+    }
+]
+console.table(productosMuestra);
